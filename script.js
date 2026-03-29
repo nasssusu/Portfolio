2c1f16d389dee56b597f2662f3285c472f4784fc
nasssusu <esh4224@gmail.com>
2026-03-29 22:38:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,91 @@
-function showMessage() {
-    alert("Your website is working 🚀");
+// typing animation
+
+const words = [
+"Web Developer",
+"Frontend Developer",
+"JavaScript Enthusiast"
+]
+
+let i = 0
+let j = 0
+let currentWord = ""
+let isDeleting = false
+
+
+function type(){
+
+currentWord = words[i]
+
+if(isDeleting){
+
+document.getElementById("typing").textContent =
+currentWord.substring(0,j--)
+
+if(j < 0){
+
+isDeleting = false
+i++
+
+if(i >= words.length){
+i = 0
 }
+
+}
+
+}
+
+else{
+
+document.getElementById("typing").textContent =
+currentWord.substring(0,j++)
+
+if(j > currentWord.length){
+
+isDeleting = true
+
+setTimeout(type,1000)
+
+return
+
+}
+
+}
+
+setTimeout(type,100)
+
+}
+
+type()
+
+
+
+// reveal on scroll animation
+
+function reveal(){
+
+const reveals =
+document.querySelectorAll(".reveal")
+
+for(let i=0;i<reveals.length;i++){
+
+const windowHeight = window.innerHeight
+
+const elementTop =
+reveals[i].getBoundingClientRect().top
+
+const elementVisible = 100
+
+if(elementTop <
+windowHeight - elementVisible){
+
+reveals[i].classList.add("active")
+
+}
+
+}
+
+}
+
+
+window.addEventListener("scroll", reveal)
+reveal()
